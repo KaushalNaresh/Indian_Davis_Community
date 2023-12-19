0236e2179ea37a939a3e7e9b3fca1eb301015cdc
KaushalNaresh <kaushalnaresh2689@gmail.com>
2023-12-20 04:01:21 +0530

    restructured the code according to MVC approach

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-
-// const connectdb = require("./mongodb");
-
-// const todoRoute = require("./routes/todoRoute");
-
+const connectdb = require("./mongodb");
+const router = express.Router();
 const app = express();
+const authRouter = require("./routes/authRoutes");
 
-app.use(express.json());
+app.use(express.json());  // to tell server that data received will be in json format
 app.use(cors());
 
-// connectdb();
+connectdb();
 
-// app.use("/api", todoRoute);
+
+app.use('/api', router);
+app.use("/api/user", authRouter)
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
