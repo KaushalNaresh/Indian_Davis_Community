dc6b669a46071bbac45bba52752318839be25ebc
KaushalNaresh <kaushalnaresh2689@gmail.com>
2023-12-19 14:31:45 +0530

    Banner and Nav updated

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,13 @@ import './Header.css'; // Make sure you link the CSS file for styling
 
 const Header = () => (
     <header className="header">
-        <div className="header__logo">ICD</div>
-        <div className="header__search">
+        <div className="header-logo">ICD</div>
+        <div className="header-search">
             <input type="text" placeholder="search" />
         </div>
-        <div className="header__actions">
-            <button className="action__button">get app</button>
-            <button className="action__button">Login</button>
+        <div className="header-actions">
+            <button className="action-button">get app</button>
+            <button className="action-button">Login</button>
         </div>
     </header>
 );
--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './HeroSection.css'; // Link to the CSS file
+import heroImage from "./images/HeroImage.jpg"
 
-const HeroSection = ({ backgroundImage }) => (
-    <div className="hero-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div className="hero-content">
-            <h1>Banner with UCD ICD information</h1>
-            <p>A little write-up about what the app is and any additional quirky things/info.</p>
-            <button className="join-button">Join ICD</button>
+function HeroSection() {
+    return (
+        <div className="hero-section">
+            <div className="hero-content">
+                <h1>Find your Desi family at UC Davis together</h1>
+                <p>One community infinite possibilities</p>
+                <button className="join-button">Join ICD</button>
+            </div>
+            <div className='hero-image'>
+                <img src={heroImage} />
+            </div>
         </div>
-    </div>
-);
+)};
 
 export default HeroSection;
