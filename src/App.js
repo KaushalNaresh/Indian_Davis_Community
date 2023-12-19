d785e0107e710c9ca315018083fd7eb01e503084
Rutuja <kalerutuja2017@gmail.com>
2023-12-19 17:58:03 +0530

    Signup Form added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React from 'react';
+// import React from 'react';
+// import Header from './Header';
+// import HeroSection from './HeroSection';
+// import Categories from './Categories';
+// import Footer from './Footer';
+
+// import './App.css';
+
+// function App() {
+  
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <HeroSection />
+//       <Categories />
+//       <Footer />
+
+//     </div>
+//   );
+// }
+
+// export default App;
+// App.js
+import React, { useState } from 'react';
 import Header from './Header';
 import HeroSection from './HeroSection';
 import Categories from './Categories';
 import Footer from './Footer';
+import SignupForm from './SignupForm'; // Import the SignupForm component
 import './App.css';
 
 function App() {
+  const [showSignup, setShowSignup] = useState(false);
+
+  const handleJoinClick = () => {
+    setShowSignup(true);
+  };
 
   return (
     <div className="App">
-      <Header />
-      <HeroSection />
-      <Categories />
-      <Footer />
-
+      
+      {showSignup ? (
+        <SignupForm />
+      ) : (
+        <>
+          <Header />
+          <HeroSection  onJoinClick={handleJoinClick} />
+          <Categories />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
